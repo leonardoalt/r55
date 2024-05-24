45aa20972fd93812fcca2cdbc8f556f5828bf5a3
Lucas Clemente Vella <lvella@gmail.com>
2024-05-24 19:14:18 +0100

    Simplest runtime example that returns 5 in 64-bit little-endian

---
+++ asm-runtime-example/runtime.s
@@ -0,0 +1,13 @@
+	.text
+	.globl _start
+_start:
+	li t0, 0
+	la a0, ret_var
+	li a1, 8
+	ecall # return(ret_var, 8), returns 0x0000000000000005 to the host
+	j _start
+
+	.data
+	.align 3
+ret_var:
+	.dword 5
